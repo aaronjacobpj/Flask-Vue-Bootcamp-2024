edfec59f3434dfa08256e80eb619482420f8d80a
AARON JACOB <aaronjacob@AARONs-MacBook-Air-2.local>
2024-11-14 08:17:28 +0530

    Day 5

--- src/store/index.js
+++ src/store/index.js
@@ -59,6 +59,13 @@ const store = createStore({
                             "slides": "",
                             "name": "Web Development Bootcamp Using Flask and Vue - Day 4"
                         },
+                                                {
+                            "source-code": "day5.zip",
+                            "youtube-video-id": "MfCkRzA_r1c",
+                            "notes": "",
+                            "slides": "",
+                            "name": "Web Development Bootcamp Using Flask and Vue - Day 5"
+                        },
                     ]
                 }
 
