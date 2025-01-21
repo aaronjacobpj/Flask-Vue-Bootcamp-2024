1ac78bad4802058883f69662bd46fbf704d60c23
AARON JACOB <aaronjacob@AARONs-MacBook-Air.local>
2025-01-21 20:31:52 +0530

    Jan 2025 day0

---
+++ src/assets/2024/Vue/November.js
@@ -0,0 +1,106 @@
+{
+    name: "Vue",
+    "base-url": "/weeks/bootcamp/2024/November/MAD ll/",
+    term: "November",
+    year: 2024,
+    sessions: [
+        {
+            "source-code": "day0.zip",
+            "youtube-video-id": "MS7qnmIJjfg",
+            "notes": "",
+            "slides": "",
+            "name": "Web Development Bootcamp Using Flask and Vue - Day 0"
+        },
+        {
+            "source-code": "day1.zip",
+            "youtube-video-id": "QMDiaSZ5eNc",
+            "notes": "",
+            "slides": "",
+            "name": "Web Development Bootcamp Using Flask and Vue - Day 1"
+        },
+        {
+            "source-code": "day2.zip",
+            "youtube-video-id": "H5CJmEVwBY0",
+            "notes": "",
+            "slides": "",
+            "name": "Web Development Bootcamp Using Flask and Vue - Day 2"
+        },
+        {
+            "source-code": "day3.zip",
+            "youtube-video-id": "d5qhNjwgMzA",
+            "notes": "",
+            "slides": "",
+            "name": "Web Development Bootcamp Using Flask and Vue - Day 3"
+        },
+        {
+            "source-code": "day4.zip",
+            "youtube-video-id": "mzn-dTY72Zg",
+            "notes": "",
+            "slides": "",
+            "name": "Web Development Bootcamp Using Flask and Vue - Day 4"
+        },
+        {
+            "source-code": "day5.zip",
+            "youtube-video-id": "MfCkRzA_r1c",
+            "notes": "",
+            "slides": "",
+            "name": "Web Development Bootcamp Using Flask and Vue - Day 5"
+        },
+        {
+            "source-code": "day6.zip",
+            "youtube-video-id": "WS_UOnStPgQ",
+            "notes": "",
+            "slides": "",
+            "name": "Web Development Bootcamp Using Flask and Vue - Day 6"
+        },
+        {
+            "source-code": "day7.zip",
+            "youtube-video-id": "8xj-XPgLS_M",
+            "notes": "",
+            "slides": "",
+            "name": "Web Development Bootcamp Using Flask and Vue - Day 7"
+        },
+        {
+            "source-code": "day8.zip",
+            "youtube-video-id": "dP_SXI43LMQ",
+            "notes": "",
+            "slides": "",
+            "name": "Web Development Bootcamp Using Flask and Vue - Day 8"
+        },
+        {
+            "source-code": "day9.zip",
+            "youtube-video-id": "3DdrlLEdG54",
+            "notes": "",
+            "slides": "",
+            "name": "Web Development Bootcamp Using Flask and Vue - Day 9"
+        },
+        {
+            "source-code": "day10.zip",
+            "youtube-video-id": "y1RWmw2UYAQ",
+            "notes": "",
+            "slides": "",
+            "name": "Web Development Bootcamp Using Flask and Vue - Day 10"
+        },
+        {
+            "source-code": "day11.zip",
+            "youtube-video-id": "GDMmVEdwvmA",
+            "notes": "",
+            "slides": "",
+            "name": "Web Development Bootcamp Using Flask and Vue - Day 11"
+        },
+        {
+            "source-code": "day12.zip",
+            "youtube-video-id": "GDMmVEdwvmA",
+            "notes": "",
+            "slides": "",
+            "name": "Web Development Bootcamp Using Flask and Vue - Day 12 (Appologies the video export failed.)"
+        },
+        {
+            "source-code": "caching-and-celery-old.zip",
+            "youtube-video-id": "COTr9u2Eb5I",
+            "notes": "",
+            "slides": "",
+            "name": "Caching and Celery"
+        },
+    ]
+}
--- src/store/index.js
+++ src/store/index.js
@@ -6,6 +6,8 @@ import {default as vuem24} from "@/assets/2024/Vue/March.json";
 import {default as vuejuly24} from "@/assets/2024/Vue/July.json";
 import {default as flasks24} from "@/assets/2024/Flask/September.json";
 import {default as vues24} from "@/assets/2024/Vue/September.json";
+import {default as vuen24} from "@/assets/2024/Vue/November.json";
+
 
 const store = createStore({
     state() {
@@ -18,109 +20,19 @@ const store = createStore({
                 vuejuly24,
                 flasks24,
                 vues24,
+                vuen24,
                 {
-                    name: "Vue",
-                    "base-url": "/weeks/bootcamp/2024/November/MAD ll/",
-                    term: "November",
-                    year: 2024,
+                    name: "Flask",
+                    "base-url": "/weeks/bootcamp/2025/January/MAD l/",
+                    term: "January",
+                    year: 2025,
                     sessions: [
                         {
                             "source-code": "day0.zip",
-                            "youtube-video-id": "MS7qnmIJjfg",
+                            "youtube-video-id": "eClCcpUt37s",
                             "notes": "",
                             "slides": "",
-                            "name": "Web Development Bootcamp Using Flask and Vue - Day 0"
-                        },
-                        {
-                            "source-code": "day1.zip",
-                            "youtube-video-id": "QMDiaSZ5eNc",
-                            "notes": "",
-                            "slides": "",
-                            "name": "Web Development Bootcamp Using Flask and Vue - Day 1"
-                        },
-                        {
-                            "source-code": "day2.zip",
-                            "youtube-video-id": "H5CJmEVwBY0",
-                            "notes": "",
-                            "slides": "",
-                            "name": "Web Development Bootcamp Using Flask and Vue - Day 2"
-                        },
-                        {
-                            "source-code": "day3.zip",
-                            "youtube-video-id": "d5qhNjwgMzA",
-                            "notes": "",
-                            "slides": "",
-                            "name": "Web Development Bootcamp Using Flask and Vue - Day 3"
-                        },
-                        {
-                            "source-code": "day4.zip",
-                            "youtube-video-id": "mzn-dTY72Zg",
-                            "notes": "",
-                            "slides": "",
-                            "name": "Web Development Bootcamp Using Flask and Vue - Day 4"
-                        },
-                        {
-                            "source-code": "day5.zip",
-                            "youtube-video-id": "MfCkRzA_r1c",
-                            "notes": "",
-                            "slides": "",
-                            "name": "Web Development Bootcamp Using Flask and Vue - Day 5"
-                        },
-                        {
-                            "source-code": "day6.zip",
-                            "youtube-video-id": "WS_UOnStPgQ",
-                            "notes": "",
-                            "slides": "",
-                            "name": "Web Development Bootcamp Using Flask and Vue - Day 6"
-                        },
-                        {
-                            "source-code": "day7.zip",
-                            "youtube-video-id": "8xj-XPgLS_M",
-                            "notes": "",
-                            "slides": "",
-                            "name": "Web Development Bootcamp Using Flask and Vue - Day 7"
-                        },
-                        {
-                            "source-code": "day8.zip",
-                            "youtube-video-id": "dP_SXI43LMQ",
-                            "notes": "",
-                            "slides": "",
-                            "name": "Web Development Bootcamp Using Flask and Vue - Day 8"
-                        },
-                        {
-                            "source-code": "day9.zip",
-                            "youtube-video-id": "3DdrlLEdG54",
-                            "notes": "",
-                            "slides": "",
-                            "name": "Web Development Bootcamp Using Flask and Vue - Day 9"
-                        },
-                        {
-                            "source-code": "day10.zip",
-                            "youtube-video-id": "y1RWmw2UYAQ",
-                            "notes": "",
-                            "slides": "",
-                            "name": "Web Development Bootcamp Using Flask and Vue - Day 10"
-                        },
-                        {
-                            "source-code": "day11.zip",
-                            "youtube-video-id": "GDMmVEdwvmA",
-                            "notes": "",
-                            "slides": "",
-                            "name": "Web Development Bootcamp Using Flask and Vue - Day 11"
-                        },
-                        {
-                            "source-code": "day12.zip",
-                            "youtube-video-id": "GDMmVEdwvmA",
-                            "notes": "",
-                            "slides": "",
-                            "name": "Web Development Bootcamp Using Flask and Vue - Day 12 (Appologies the video export failed.)"
-                        },
-                        {
-                            "source-code": "caching-and-celery-old.zip",
-                            "youtube-video-id": "COTr9u2Eb5I",
-                            "notes": "",
-                            "slides": "",
-                            "name": "Caching and Celery"
+                            "name": "Web Development Bootcamp Using Flask and HTML - Day 0"
                         },
                     ]
                 }
